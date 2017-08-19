7f4cb53ce6bd0e8089f8a06c59994a49dde2e763
princessV <puddingtea07@hotmail.com>
2017-08-19 15:39:44 +0800

    lc array #9

---
+++ lc/33_Search_in_Rotated_Sorted_Array.js
@@ -0,0 +1,30 @@
+/**
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+You may assume no duplicate exists in the array.
+ */
+
+var search = function(nums, target) {
+    let low = 0, high = nums.length;
+
+    while (low < high) {
+        let mid = parseInt((low + high) / 2);
+        let num = ((target < nums[0]) === (nums[mid] < nums[0])) ? nums[mid] : (target < nums[0] ? Number.NEGATIVE_INFINITY : Number.POSITIVE_INFINITY);
+
+        if (num > target) {
+            high = mid;
+        } else if (num < target) {
+            low = mid + 1;
+        } else {
+            return mid;
+        }
+    }
+
+    return -1;
+};
+
+console.log(search([3, 1], 3));
