9f7f164cd6026a471a46a13a2009118f28dd4820
Qiyang Gu <puddingtea07@hotmail.com>
2017-10-11 00:07:14 -0400

    lc #13

---
+++ lc/A024_Swap_Nodes_in_Pairs.js
@@ -0,0 +1,20 @@
+/**
+Given a linked list, swap every two adjacent nodes and return its head.
+
+For example,
+Given 1->2->3->4, you should return the list as 2->1->4->3.
+
+Your algorithm should use only constant space. You may not modify the values in the list, only nodes itself can be changed.
+ */
+
+const ListNode = require('../datastructure/ListNode')
+
+var swapPairs = function(head) {
+    if (!head) return null;
+    let p1 = head, p2 = p1.next;
+    if (!p2) return p1;
+    let p3 = p2.next;
+    p2.next = p1;
+    p1.next = swapPairs(p3);
+    return p2;
+};
