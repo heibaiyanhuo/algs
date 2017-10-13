9e544c294d0e2ddf270335e9315ac723df6ef989
Qiyang Gu <puddingtea07@hotmail.com>
2017-10-12 21:40:08 -0400

    format file name

---
+++ lc/A003_Longest_Substring_Without_Repeating_Characters.js
@@ -0,0 +1,14 @@
+
+
+var lengthOfLongestSubstring = (s) => {
+    let res = 0, from = 0;
+    let m = new Map();
+    for (let i = 0; i < s.length; i++) {
+        if (m.has(s.charAt(i))) from = Math.max(from, m.get(s.charAt(i) ) + 1);
+        m.set(s.charAt(i), i);
+        res = Math.max(res, i - from + 1);
+    }
+    return res;
+}
+
+console.log(lengthOfLongestSubstring('abcbbbefg'));
