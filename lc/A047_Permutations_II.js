af5bb392e77321cb4c6d2036b5f97fca0d9359e4
Qiyang Gu <puddingtea07@hotmail.com>
2017-10-13 01:29:57 -0400

    lc #16

---
+++ lc/A047_Permutations_II.js
@@ -0,0 +1,29 @@
+
+var permuteUnique = function(nums) {
+    let result = [], one = [];
+    // let usedMap = new Map();
+    let usedSet = new Set();
+    nums.sort();
+    backtrack(result, one, nums, usedSet);
+    return result;
+};
+
+
+const backtrack = (result, one, nums, usedSet) => {
+    if (one.length === nums.length) {
+        result.push(one.slice());
+        return;
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        if (usedSet.has(i) || (i > 0 && nums[i] === nums[i-1] && !usedSet.has(i - 1))) continue;
+        usedSet.add(i);
+        one.push(nums[i]);
+        backtrack(result, one, nums, usedSet);
+        one.pop();
+        usedSet.delete(i);
+    }
+    return;
+}
+
+console.log(permuteUnique([3, 3, 0, 3]));
