82dd994a5f878e42ac0ac973a040daa46ad5bea0
Qiyang Gu <puddingtea07@hotmail.com>
2017-12-06 15:05:10 -0500

    lc #46

---
+++ lc/A206_Reverse_Linked_List.js
@@ -0,0 +1,27 @@
+/**
+ * Reverse a singly linked list.
+ */
+const ListNode = require('./utils').ListNode;
+const arrTo = require('./utils').arrayToLinkedList;
+const showList = require('./utils').showLinkedList;
+
+// iterative
+const reverseList = (head) => {
+    let rev = null;
+    let p = head;
+    while (p) {
+        [rev, rev.next, p] = [p, rev, p.next];
+    }
+    return rev;
+}
+
+// recursive
+const reverseList2 = (head) => {
+    if (!head || !head.next) return head;
+    let nextNode = head.next;
+    let rev = reverseList2(nextNode);
+    nextNode.next = head;
+    head.next = null;
+    return rev;
+}
+showList(reverseList2(arrTo([1, 2, 4, 3, 5])))
