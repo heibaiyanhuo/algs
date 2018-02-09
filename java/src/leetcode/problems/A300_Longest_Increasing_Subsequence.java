e847e129b3b4576668246bf714402afafeec9c16
Qiyang Gu <puddingtea07@hotmail.com>
2018-02-08 23:31:25 -0500

    lc java

---
+++ java/src/leetcode/problems/A300_Longest_Increasing_Subsequence.java
@@ -0,0 +1,24 @@
+package leetcode.problems;
+
+public class A300_Longest_Increasing_Subsequence {
+
+    public int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[0] = 1;
+        int res = 1;
+        for (int i = 1; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], 1 + dp[j]);
+                }
+            }
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+}
