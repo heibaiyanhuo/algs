4394e9a5f09c48ae6808470505ebf0de4afdfbbc
Qiyang Gu <guqiyang@megvii.com>
2018-06-07 19:43:14 +0800

    add utils

---
+++ java/src/leetcode/utils/Serializer.java
@@ -0,0 +1,33 @@
+package leetcode.utils;
+
+import leetcode.datastructures.TreeNode;
+
+public class Serializer {
+
+    /**
+     * Use level order to serialize a tree.
+     * @param root, the root node of a tree
+     * @return an string like "[1, 2, null]"
+     */
+    public static String serialize(TreeNode root) {
+
+        return null;
+    }
+
+
+    public static TreeNode deserialize(String sequence) {
+        String[] nodesVals = sequence.replaceAll("\\s+", "").split(",");
+        return createTreeNode(0, nodesVals);
+    }
+
+
+    private static TreeNode createTreeNode(int index, String[] nodesVals) {
+        if (index >= nodesVals.length || nodesVals[index] == "null") {
+            return null;
+        }
+        TreeNode node = new TreeNode(Integer.parseInt(nodesVals[index]));
+        node.left = createTreeNode(2 * index + 1, nodesVals);
+        node.right = createTreeNode(2 * index + 2, nodesVals);
+        return node;
+    }
+}
---
+++ java/src/leetcode/utils/Test.java
@@ -0,0 +1,10 @@
+package leetcode.utils;
+
+
+public class Test {
+
+
+    public static void main(String[] args) {
+
+    }
+}
