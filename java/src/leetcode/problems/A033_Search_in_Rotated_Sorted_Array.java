b388f350fca6d33eb6a06bb737912a1152e1fb34
Qiyang Gu <guqiyang@megvii.com>
2018-06-20 21:54:09 +0800

    Add solution for 33, 389

---
+++ java/src/leetcode/problems/A033_Search_in_Rotated_Sorted_Array.java
@@ -0,0 +1,49 @@
+package leetcode.problems;
+
+public class A033_Search_in_Rotated_Sorted_Array {
+
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int lo = 0;
+        int hi = nums.length - 1;
+
+        while (lo + 1 < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                if (nums[mid] > nums[lo]) {
+                    if (target >= nums[lo]) {
+                        hi = mid - 1;
+                    } else {
+                        lo = mid + 1;
+                    }
+                } else {
+                    hi = mid - 1;
+                }
+            } else {
+                if (nums[mid] > nums[lo]) {
+                    lo = mid + 1;
+                } else {
+                    if (target >= nums[lo]) {
+                        hi = mid - 1;
+                    } else {
+                        lo = mid + 1;
+                    }
+                }
+            }
+        }
+
+        if (nums[lo] == target) {
+            return lo;
+        }
+
+        if (nums[lo + 1] == target) {
+            return lo + 1;
+        }
+
+        return -1;
+    }
+}
