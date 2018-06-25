50ce3f803834ffcc22b36fa5e4d3c696020a2167
Qiyang Gu <guqiyang@megvii.com>
2018-06-25 19:02:38 +0800

    Add solutions for 240, 274, 275, 392

---
+++ java/src/leetcode/problems/A240_Search_a_2D_Matrix_II.java
@@ -0,0 +1,25 @@
+package leetcode.problems;
+
+public class A240_Search_a_2D_Matrix_II {
+
+    private boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int i = 0, j = cols - 1;
+        while (i < rows && j >= 0) {
+            if (matrix[i][j] == target) {
+                return true;
+            }
+            if (matrix[i][j] < target) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return false;
+    }
+}
