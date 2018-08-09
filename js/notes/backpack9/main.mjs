bb8dfd96a7a4c70f6a2ce881336d3690a2569990
Qiyang Gu <puddingtea07@hotmail.com>
2018-08-10 00:29:48 +0800

    Update backpack9

--- js/notes/backpack9/main.mjs
+++ js/notes/backpack9/main.mjs
@@ -1,4 +1,5 @@
 import { zeroOnePack } from './bp1';
+import { completePack } from './bp2';
 
 
 const start = () => {
@@ -6,7 +7,7 @@ const start = () => {
   const costs = [4, 6, 11, 15, 20];
   const worths = [2, 3, 5, 7, 1];
 
-  console.log(zeroOnePack(limit, costs, worths));
+  console.log(completePack(limit, costs, worths));
 }
 
 start();
