f95cd32a69711709a44247c6544e91af048a05f2
Qiyang Gu <puddingtea07@hotmail.com>
2018-09-05 23:57:01 -0400

    Add SweepingPoint

---
+++ java/src/leetcode/datastructures/SweepingPoint.java
@@ -0,0 +1,10 @@
+package leetcode.datastructures;
+
+public class SweepingPoint {
+    public int val;
+    public int label;
+    public SweepingPoint(int val, int label) {
+        this.val = val;
+        this.label = label;
+    }
+}
