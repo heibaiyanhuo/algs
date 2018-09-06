030f014e95f9ce733d2fb55c4945d078ca1ee74c
Qiyang Gu <puddingtea07@hotmail.com>
2018-09-05 23:56:30 -0400

    Add solution for 30

--- java/src/leetcode/problems/A030_Substring_with_Concatenation_of_All_Words.java
+++ java/src/leetcode/problems/A030_Substring_with_Concatenation_of_All_Words.java
@@ -41,4 +41,39 @@ public class A030_Substring_with_Concatenation_of_All_Words {
         }
         return res;
     }
+
+    public List<Integer> findSubstring2(String s, String[] words) {
+        List<Integer> res = new ArrayList<>();
+        if (s == null || s.length() == 0 || words == null || words.length == 0) {
+            return res;
+        }
+        int wordLength = words[0].length();
+        int N = words.length;
+        int sLength = s.length();
+        int windowLength = wordLength * N;
+        Map<String, Integer> wordMap = new HashMap<>();
+        for (String word: words) {
+            wordMap.put(word, wordMap.getOrDefault(word, 0) + 1);
+        }
+        Map<String, Integer> visited = new HashMap<>();
+        for (int i = 0; i < wordLength; i++) {
+            for (int j = i; j + windowLength <= sLength; j+=wordLength) {
+                String window = s.substring(j, j + windowLength);
+                for (int k = 0; k < N; k++) {
+                    String word = window.substring(j + k * wordLength, j + (k + 1) * wordLength);
+                    int count = visited.getOrDefault(word, 0) + 1;
+                    if (count > wordMap.getOrDefault(word, 0)) {
+                        j += k * wordLength;
+                        break;
+                    } else if (k == N - 1) {
+                        res.add(j);
+                    } else {
+                        visited.put(word, count);
+                    }
+                }
+                visited.clear();
+            }
+        }
+        return res;
+    }
 }
